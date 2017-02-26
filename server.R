cdefd2674718ac3bdf25c6cd4c8efdd160716c4d
ttimbers <tiffany.timbers@gmail.com>
2017-02-26 07:13:43 -0800

    Fixed issue with data not matching province on map and missing vals for Que

--- server.R
+++ server.R
@@ -10,26 +10,28 @@ library(tidyverse)
 library(leaflet)
 library(rgdal)
 
+# load data
 canada <- readOGR("data/CanadianProvinces.kml",
                   "CanadianProvinces3",
                   encoding="utf-8")
+
+horse_pop <- read_csv("data/00030067-eng.csv")
+horse_pop <- filter(horse_pop,
+                    DATE == "At June 1 (x 1,000)")
+horse_pop$Value <- horse_pop$Value * 1000
+
 
 shinyServer(function(input, output) {
   
   output$horse_pop_Plot <- renderPlot({
 
-    # generate bins based on input$bins from ui.R
-    horse_pop <- read_csv("data/00030067-eng.csv")
-    horse_pop <- filter(horse_pop,
-                        DATE == "At June 1 (x 1,000)")
-    horse_pop$Value <- horse_pop$Value * 1000
-    
     # get region of interest from input
     region <- input$region
 
     # draw the line plot for the specified region
     horse_pop_region <- filter(horse_pop,
                                GEO == region)
+    
     ggplot(horse_pop_region, aes(x = Ref_Date, y = Value)) +
       geom_point() +
       geom_line() +
@@ -40,34 +42,30 @@ shinyServer(function(input, output) {
   })
   
   output$horse_pop_map <- renderLeaflet({
-    horse_pop <- read_csv("data/00030067-eng.csv")
-    horse_pop <- filter(horse_pop,
-                        DATE == "At June 1 (x 1,000)")
-    horse_pop$Value <- horse_pop$Value * 1000
-
     # get year of interest from input
     year <- input$year
 
-    #load base map
-    # canada <- readOGR("data/CanadianProvinces.kml",
-    #                   "CanadianProvinces3",
-    #                   encoding="utf-8")
-
-    # draw map for that year
+    #filter to desired year
     horse_pop_year <- filter(horse_pop,
                              Ref_Date == year,
-                             GEO != "Canada")
+                             GEO != "Canada") %>% 
+      select(GEO, Value)
 
+    # merge data with canada data
+    canada_year <- merge(canada, horse_pop_year, by.x = "Name", by.y = "GEO")
+    
     pal <- colorQuantile("YlGn", NULL, n = 5)
 
+    # create pop_up data
     prov_popup <- paste0("<strong>Province: </strong>",
-                         horse_pop_year$GEO,
+                         canada_year@data$Name,
                           "<br><strong>Number of horses: </strong>",
-                         horse_pop_year$Value)
+                         canada_year@data$Value)
 
-    leaflet(data = canada) %>%
+    # create map
+    leaflet(data = canada_year) %>%
       addProviderTiles("CartoDB.Positron") %>%
-      addPolygons(fillColor = ~pal(horse_pop_year$Value),
+      addPolygons(fillColor = ~pal(Value),
                   fillOpacity = 0.8,
                   color = "#BDBDC3",
                   weight = 1,
